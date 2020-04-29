f1776338e98e1a99e81b524cef041d43f9048c4b
Greg Allebach <greg.allebach@gmail.com>
2020-04-29 18:06:33 -0400

    added name and modified border around pictures; made individual variables into array of objects

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,66 +1,46 @@
-var kylo = {
-    name: "kylo-ren",
-    imageBox: $("<div class='imagebox'></div>"),
-    pic: $("<img src='assets/images/kylo-ren.jpg' alt='kylo-ren' id='kylo-pic' class='character-button'>"),
-    healthDisplay: $("<div class='image-textbox' id='kylo-ren'></div>"),
-    healthPoints: 150,
-    attackPoints: 10,
-    counterAttack: 20
-}
+var characters = [ 
+    kylo = {
+        name: "Kylo Ren",
+        nameBox: $("<div class='name-box'</div>"),
+        imageBox: $("<div class='imagebox'></div>"),
+        pic: $("<img src='assets/images/kylo-ren.jpg' alt='Kylo Ren' id='kylo-pic' class='character-button'>"),
+        healthDisplay: $("<div class='image-textbox' id='kylo-ren'></div>"),
+        healthPoints: 150,
+        attackPoints: 10,
+        counterAttack: 20
+    },
+    rey = {
+        name: "Rey",
+        nameBox: $("<div class='name-box'</div>"),
+        imageBox: $("<div class='imagebox'></div>"),
+        pic: $("<img src='assets/images/rey.jpg' alt='Rey' id='rey-pic' class='character-button'>"),
+        healthDisplay: $("<div class='image-textbox' id='rey'></div>"),
+        healthPoints: 105,
+        attackPoints: 10,
+        counterAttack: 20
+    },
+    finn = {
+        name: "Finn",
+        nameBox: $("<div class='name-box'</div>"),
+        imageBox: $("<div class='imagebox'></div>"),
+        pic: $("<img src='assets/images/finn.jpg' alt='Finn' id='finn-pic' class='character-button'>"),
+        healthDisplay: $("<div class='image-textbox' id='finn'></div>"),
+        healthPoints: 101,
+        attackPoints: 10,
+        counterAttack: 20
+    },
+    babyYoda = {
+        name: "Baby Yoda",
+        nameBox: $("<div class='name-box'</div>"),
+        imageBox: $("<div class='imagebox'></div>"),
+        pic: $("<img src='assets/images/baby-yoda.jpg' alt='Baby Yoda' id='yoda-pic' class='character-button'>"),
+        healthDisplay: $("<div class='image-textbox' id='baby-yoda'></div>"),
+        healthPoints: 102,
+        attackPoints: 10,
+        counterAttack: 20
+    }
+]
 
-// kylo.healthDisplay.text("Health: " + kylo.healthPoints);
-// $(".pick-character").append(kylo.imageBox);
-// kylo.imageBox.append(kylo.healthDisplay);
-// kylo.imageBox.append(kylo.pic);
-
-
-
-var rey = {
-    name: "rey",
-    imageBox: $("<div class='imagebox'></div>"),
-    pic: $("<img src='assets/images/rey.jpg' alt='rey' id='rey-pic' class='character-button'>"),
-    healthDisplay: $("<div class='image-textbox' id='rey'></div>"),
-    healthPoints: 105,
-    attackPoints: 10,
-    counterAttack: 20
-}
-
-// rey.healthDisplay.text("Health: " + rey.healthPoints);
-// $(".pick-character").append(rey.imageBox);
-// rey.imageBox.append(rey.healthDisplay);
-// rey.imageBox.append(rey.pic);
-
-var finn = {
-    name: "finn",
-    imageBox: $("<div class='imagebox'></div>"),
-    pic: $("<img src='assets/images/finn.jpg' alt='finn' id='finn-pic' class='character-button'>"),
-    healthDisplay: $("<div class='image-textbox' id='finn'></div>"),
-    healthPoints: 101,
-    attackPoints: 10,
-    counterAttack: 20
-}
-
-// finn.healthDisplay.text("Health: " + finn.healthPoints);
-// $(".pick-character").append(finn.imageBox);
-// finn.imageBox.append(finn.healthDisplay);
-// finn.imageBox.append(finn.pic);
-
-var babyYoda = {
-    name: "baby-yoda",
-    imageBox: $("<div class='imagebox'></div>"),
-    pic: $("<img src='assets/images/baby-yoda.jpg' alt='baby-yoda' id='yoda-pic' class='character-button'>"),
-    healthDisplay: $("<div class='image-textbox' id='baby-yoda'></div>"),
-    healthPoints: 102,
-    attackPoints: 10,
-    counterAttack: 20
-}
-
-// babyYoda.healthDisplay.text("Health: " + babyYoda.healthPoints);
-// $(".pick-character").append(babyYoda.imageBox);
-// babyYoda.imageBox.append(babyYoda.healthDisplay);
-// babyYoda.imageBox.append(babyYoda.pic);
-
-var characters = [kylo,rey,finn,babyYoda];
 var yourCharacter;
 var yourEnemies = [];
 var defender;
@@ -70,9 +50,11 @@ resetCharacters();
 
 function resetCharacters() {    
     for (var i = 0; i < characters.length; i++) {
+        characters[i].nameBox.text(characters[i].name);
         characters[i].healthDisplay.text("Health: " + characters[i].healthPoints);
         $(".pick-character").append(characters[i].imageBox);
         characters[i].imageBox.append(characters[i].healthDisplay);
+        characters[i].imageBox.append(characters[i].nameBox);
         characters[i].imageBox.append(characters[i].pic);
     }
 }
@@ -101,48 +83,51 @@ $(".character-button").on("click", function() {
                 $(".defender-section").append(characters[i].imageBox);
                 $(characters[i].imageBox).append(characters[i].healthDisplay);
                 $(characters[i].imageBox).append(characters[i].pic);
+                $(".fight-section").empty();
+                $(".fight-section").append("<button class='btn btn-danger' id='attack-button'>Attack</button>");
             }
         }   
     }
+
+    $("#attack-button").on("click", function() {
+        if (yourCharacter.healthPoints > 0 && defender.healthPoints > 0) {
+
+            yourCharacter.healthPoints = yourCharacter.healthPoints - defender.counterAttack;
+
+            defender.healthPoints = defender.healthPoints - yourCharacter.attackPoints;
+
+            yourCharacter.healthDisplay.text("Health: " + yourCharacter.healthPoints);
+
+            defender.healthDisplay.text("Health: " + defender.healthPoints);
+            
+            $(".attack-results").text("You attacked " + defender.name + " for " + yourCharacter.attackPoints + " damage ");
+            $(".attack-results2").text(defender.name + " attacked you back for " + defender.counterAttack + " damage");
+
+            yourCharacter.attackPoints += 6;
+            
+        }
+        else if (defender.healthPoints <= 0 && yourCharacter.healthPoints > 0 && yourEnemies !== defeatedEnemies) {        
+            $(".defender-section").empty();
+            $(".attack-results").text("You defeated " + defender.name);
+            $(".attack-results2").empty();
+            defeatedEnemies += defender;
+            defender = null;
+            $(".fight-section").empty();
+            $(".fight-section").append("<button class='btn btn-danger' id='attack-button' disabled>Attack</button>");
+        }
+        else if (defender.healthPoints <= 0 && yourCharacter.healthPoints > 0 && yourEnemies == defeatedEnemies) {
+            $(".defender-section").empty();
+            $(".attack-results").text("You defeated all your enemies!");
+            $(".attack-results2").append("<a class='btn btn-secondary' href='index.html'>Reset</a>");
+        }  
+        else if (yourCharacter.healthPoints <= 0) {
+            $(".attack-results").text("You have been defeated by " + defender.name);
+            $(".attack-results2").empty();
+            $(".attack-results2").append("<a class='btn btn-secondary' href='index.html'>Reset</a>");
+        }    
+    })
+
 })
-
-$("#attack-button").on("click", function() {
-    if (yourCharacter.healthPoints > 0 && defender.healthPoints > 0) {
-
-        yourCharacter.healthPoints = yourCharacter.healthPoints - defender.counterAttack;
-
-        defender.healthPoints = defender.healthPoints - yourCharacter.attackPoints;
-
-        yourCharacter.healthDisplay.text("Health: " + yourCharacter.healthPoints);
-
-        defender.healthDisplay.text("Health: " + defender.healthPoints);
-        
-        $(".attack-results").text("You attacked " + defender.name + " for " + yourCharacter.attackPoints + " damage ");
-        $(".attack-results2").text(defender.name + " attacked you back for " + defender.counterAttack + " damage");
-
-        yourCharacter.attackPoints += 6;
-        
-    }
-    else if (defender.healthPoints <= 0 && yourCharacter.healthPoints > 0 && yourEnemies !== defeatedEnemies) {        
-        $(".defender-section").empty();
-        $(".attack-results").text("You defeated " + defender.name);
-        $(".attack-results2").empty();
-        defeatedEnemies += defender;
-        defender = null;
-    }
-    else if (defender.healthPoints <= 0 && yourCharacter.healthPoints > 0 && yourEnemies == defeatedEnemies) {
-        $(".defender-section").empty();
-        $(".attack-results").text("You defeated all your enemies!");
-        $(".attack-results2").append("<button id='reset-button'>Reset</button>");
-    }  
-    else if (yourCharacter.healthPoints <= 0) {
-        $(".attack-results").text("You have been defeated");
-        $(".attack-results2").empty();
-        $(".attack-results2").append("<a class='btn btn-secondary' href='index.html'>Reset</a>");
-    }    
-    
-})
-
 
 
 
